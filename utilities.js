18f39953cd05c7590d9bb2011a089fdc9130c150
Nithin <nithin93@gmail.com>
2017-01-05 14:45:43 +0530

    Add files via upload

--- utilities.js
+++ utilities.js
@@ -1,11 +1,23 @@
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var _DEFAULTS = {
+
+};
+var getConnectionString = function(){
+	var secObj = JSON.parse(fs.readFileSync('dbauth.json', 'utf8'));
+	return "mongo://" + secObj.dbUser + ":" + secObj.dbPass + "@" + secObj.host + ":" + secObj.port + "/" + secObj.database;
+}
+
 exports.fetchFromDB = function(id){
 	return 'C:/Users/nithin.murali/Downloads/movie.mp4';
 }
+
 exports.validateUserExists = function(username){
 	return users.filter(function( object ) {
   		return object.username == username;
 	});
 }
+
 exports.validateLogin = function(username, password){
 	var hasUser = validateUserExists(username).length != 0;
 	if(hasUser){
@@ -17,4 +29,13 @@ exports.validateLogin = function(username, password){
 	else{
 		return false;
 	}
+}
+
+exports.persist = function(){
+	MongoClient.connect(getConnectionString(), function(err, db){
+		if(err){
+			console.log(err);
+		}
+
+	});
 }
