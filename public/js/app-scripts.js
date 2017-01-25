7c54460b21333c41dc8e31b6bae79286b26222d5
Bala Krishna <bala.alab@live.com>
2017-01-25 09:24:10 +0530

    Login UI changes

--- public/js/app-scripts.js
+++ public/js/app-scripts.js
@@ -1,12 +1,1 @@
-var log = function () {
-    $("#login-form").delay(100).fadeIn(100);
-    $("#register-form").fadeOut(100);
-    $('#register-form-link').removeClass('active');
-    $('#login-form-link').addClass('active');
-};
-var reg = function () {
-    $("#register-form").delay(100).fadeIn(100);
-    $("#login-form").fadeOut(100);
-    $('#login-form-link').removeClass('active');
-    $('#register-form-link').addClass('active');
-};
+
